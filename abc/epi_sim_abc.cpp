ea5d355139c11070970128a49a16342a6d792ff2
Thomas Hladish <thladish@login4.ranger.tacc.utexas.edu>
2011-12-27 21:56:16 -0600

    incremental commit

--- abc/epi_sim_abc.cpp
+++ abc/epi_sim_abc.cpp
@@ -16,10 +16,11 @@ struct Particle {
     int P0;
 };
 
-struct Tau_sq {
+struct Doubled_variances {
     double R0, Ih, P0, h;
 };
 
+/*
 struct ParticleSet { 
     vector<Particle> particles;
     Particle back() { return particles.back(); }
@@ -27,9 +28,9 @@ struct ParticleSet {
     int size() { return (int) particles.size(); }
     Particle& operator[] ( const int nIndex) { return particles[nIndex]; }
 };
+*/
 
-Parameters asdffdsa;
-vector<ParticleSet> theta; 
+//vector<ParticleSet> theta; 
 
 // Define ABC parameters
 const double R0_min = 1.0;
@@ -41,6 +42,16 @@ const int P0_max = 256;
 const double h_min  = 0.0;
 const double h_max  = 1.0;
 
+// Don't consider values outside of these bounds
+// (because they would be nonsensical or would cause simulator to crash)
+const double R0_hard_min = 0.0;
+const double R0_hard_max = 1000.0;
+const double Ih_hard_min = 0.00001;
+const double Ih_hard_max = 10000.0;
+const int P0_hard_min = 1;
+const int P0_hard_max = 1000;
+const double h_hard_min  = 0.0;
+const double h_hard_max  = 1.0;
 
 int NET_SIZE;     // size of network to use
 int patient_zero_ct; // number of infections to introduce (usually 1)
@@ -56,11 +67,13 @@ string observed_data_filename;
 
 void generate_network(Network* net, MultiSeason_Sim* sim);
 void connect_network (Network* net);
-void read_observed_data(string filename, char sep, map<string, vector<float> > &data);
+void load_observed_data(string filename, char sep, map<string, vector<float> > &data);
 double KS(vector<float>s1, vector<float>s2);
 void report_metrics(map<string, vector<float> > s1, map<string, vector<float> > s2);
 //double autocorrelation_score( vector<float>s1, vector<float>s2 );
 vector<double> autocorrelation_matrix( map<string, vector<float> >& data );
+Doubled_variances calculate_doubled_variances( vector<Particle> particle_set );
+vector<double> calculate_predictive_prior_weights(vector<Particle> predictive_prior, int set_num); 
 
 //float obs_R0 = 3;   // estimated from Vynnycky et al 2007. Estimates of the reproduction numbers of Spanish influenza using morbidity data
 //float obs_R  = 1.3; // from Chowell et al 2007. Seasonal Influenza in the United States, France, and Australia: Transmission and prospects for control
@@ -167,62 +180,45 @@ bool fileExists(string strFilename) {
     return(blnReturn);
 }
 
-int determine_set_number() {
-    bool found_file = true;
-    int set_num = 0;
-    while (found_file) {
-        stringstream ss;
-        ss << "predictive_prior." << set_num;
-        if ( fileExists(ss.str()) ) {
-            set_num++;
-        } else {
-            found_file = false;
-        }
-    }
-    return set_num;
-}
-
-
-void sample_prior(ParticleSet &particles, int sample_size, MTRand* mtrand) {
-    particles.resize(sample_size);
-    for (int i = 0; i < sample_size; i++) {
-        particles[i].R0 = rand_uniform( R0_min, R0_max, mtrand );
-        particles[i].Ih = rand_uniform( Ih_min, Ih_max, mtrand );
-        particles[i].h  = rand_uniform( h_min,  h_max,  mtrand  );
-        particles[i].P0 = rand_uniform_int( P0_min, P0_max, mtrand );
-    }
-}
-
-
-void sample_predictive_prior(int set_num, ParticleSet &particles, int sample_size, MTRand* mtrand) {
-    // Read in predictive prior from the last set.
-    // We need to calculate the proper weights for the predictive prior so that we know how to sample from it.
-    ParticleSet predictive_prior = read_predictive_prior_file( "predictive_prior." + to_string(set_num - 1) );
-    
-    vector<double> old_weights;
-    if (set_num == 1) {
-        // uniform weights for set 0 predictive prior
-        old_weights.resize(predictive_prior.size(), 1.0/(double) predictive_prior.size());
-    } else if ( set_num > 1 ) {
-        // weights from set - 2 are needed to calculate weights for set - 1
-        old_weights = read_weights_file("weights." + to_string(set_num - 2) );
-    }
-
-    particles.resize(sample_size);
-    for (int i = 0; i < sample_size; i++) {
-       // particles[i].R0 = rand_uniform( R0_min, R0_max, mtrand );
-       // particles[i].Ih = rand_uniform( Ih_min, Ih_max, mtrand );
-       // particles[i].h  = rand_uniform( h_min,  h_max,  mtrand  );
-       // particles[i].P0 = rand_uniform_int( P0_min, P0_max, mtrand );
-    }
-}
-
-void read_predictive_prior_file(string filename) {
+vector<Particle> read_predictive_prior_file(int set_num) {
+    string filename = "predictive_prior." + to_string(set_num);
 // 'R0', 'Ih', 'h', 'P0', 'mean', 'median', 'max', 'range', 'sd', 'skew', 'ss', 'll', 'sl', 'ls', 'Re'   
+    ifstream myfile(filename.c_str());
+    std::stringstream ss;
+    vector<Particle> predictive_prior;
 
+    if (myfile.is_open()) {
+        string line;
+        char sep = ' ';
+
+        while ( getline(myfile, line) ) {
+            vector<string> fields;
+            split(line, sep, fields);
+            Particle p;
+
+            const char whitespace[] = " \n\t\r";
+            string R0_str = strip( fields[0], whitespace ); 
+            string Ih_str = strip( fields[1], whitespace ); 
+            string h_str  = strip( fields[2], whitespace ); 
+            string P0_str = strip( fields[3], whitespace ); 
+
+            p.R0 = to_double( R0_str );
+            p.Ih = to_double( Ih_str );
+            p.h  = to_double( h_str );
+            p.P0 = to_int( P0_str );
+
+            predictive_prior.push_back(p);
+        }
+    } else {
+        cerr << "Failed to open parameter file\n";
+        exit(103);
+    }
+    return predictive_prior;
 }
 
-vector<double> read_weights_file(string filename) {
+vector<double> read_weights_file(int set_num) {
+    string filename = "weights." + to_string(set_num);
+
     vector<double> weights;
     ifstream myfile(filename.c_str());
     std::stringstream ss;
@@ -241,6 +237,86 @@ vector<double> read_weights_file(string filename) {
     return weights;
 }
 
+
+void write_weights_file(vector<double> weights, int set_num) {
+    string filename = "weights." + to_string(set_num);
+
+    ofstream pipe(filename.c_str(), ios::out);
+    for (unsigned int i = 0; i < weights.size(); i++) {
+        pipe << weights[i] << endl;
+    }
+    pipe.close();
+    return;
+}
+
+
+int determine_set_number() {
+    bool found_file = true;
+    int set_num = 0;
+    while (found_file) {
+        stringstream ss;
+        ss << "predictive_prior." << set_num;
+        if ( fileExists(ss.str()) ) {
+            set_num++;
+        } else {
+            found_file = false;
+        }
+    }
+    return set_num;
+}
+
+
+void sample_prior(vector<Particle> &particles, int sample_size, MTRand* mtrand) {
+    particles.resize(sample_size);
+    for (int i = 0; i < sample_size; i++) {
+        particles[i].R0 = rand_uniform( R0_min, R0_max, mtrand );
+        particles[i].Ih = rand_uniform( Ih_min, Ih_max, mtrand );
+        particles[i].h  = rand_uniform( h_min,  h_max,  mtrand  );
+        particles[i].P0 = rand_uniform_int( P0_min, P0_max, mtrand );
+    }
+}
+
+double rand_trunc_normal(double mu, double sigma_squared, double min, double max, MTRand* mtrand) {
+    double sigma = sqrt(sigma_squared);
+    // Don't like this, but it should work
+    while (1) {
+        double dev = mtrand->randNorm(mu, sigma);
+        if (dev >= min and dev <= max) {
+            return dev;
+        }
+    }
+}
+                                
+
+void sample_predictive_prior(int set_num, vector<Particle> &particles, int sample_size, MTRand* mtrand) {
+    // Read in predictive prior from the last set.
+    // We need to calculate the proper weights for the predictive prior so that we know how to sample from it.
+    vector<Particle> predictive_prior = read_predictive_prior_file( set_num - 1 );
+    Doubled_variances sampling_variance = calculate_doubled_variances( predictive_prior );
+    
+    vector<double> weights;
+    if (set_num == 1) {
+        // uniform weights for set 0 predictive prior
+        weights.resize(predictive_prior.size(), 1.0/(double) predictive_prior.size());
+    } else if ( set_num > 1 ) {
+        // weights from set - 2 are needed to calculate weights for set - 1
+        weights = calculate_predictive_prior_weights( predictive_prior, set_num );
+    }
+    write_weights_file( weights, set_num - 1 );
+
+    particles.resize(sample_size);
+    for (int i = 0; i < sample_size; i++) {
+        // Select a particle index j to use from the predictive prior
+        int j = rand_nonuniform_int( weights, mtrand );
+        particles[i].R0 = rand_trunc_normal( predictive_prior[ j ].R0, sampling_variance.R0, R0_hard_min, R0_hard_max, mtrand ); 
+        particles[i].Ih = rand_trunc_normal( predictive_prior[ j ].Ih, sampling_variance.Ih, Ih_hard_min, Ih_hard_max, mtrand ); 
+        particles[i].h  = rand_trunc_normal( predictive_prior[ j ].h,  sampling_variance.h,  h_hard_min,  h_hard_max,  mtrand ); 
+        particles[i].P0 = (int) (rand_trunc_normal( predictive_prior[ j ].P0, sampling_variance.P0, P0_hard_min, P0_hard_max, mtrand ) + 0.5);
+    }
+    return;
+}
+
+/*
 vector<double> determine_weights(int particles_in_prior) {
     const double N = (double) particles_in_prior;
     vector<double> weights;
@@ -258,34 +334,75 @@ vector<double> determine_weights(int particles_in_prior) {
     }
         
     return weights;
+}*/
+
+
+Doubled_variances calculate_doubled_variances( vector<Particle> particle_set ) {
+    int set_size = particle_set.size();
+    vector<double> R0_vec( set_size );
+    vector<double> Ih_vec( set_size );
+    vector<double> h_vec( set_size );
+    vector<double> P0_vec( set_size );
+
+    for (int i = 0; i < set_size; i++) {
+        R0_vec[i] = particle_set[i].R0;
+        Ih_vec[i] = particle_set[i].Ih;
+        h_vec[i]  = particle_set[i].h;
+        P0_vec[i] = particle_set[i].P0;
+    }
+
+    Doubled_variances doubled_var;
+    doubled_var.R0 = 2 * variance( R0_vec );
+    doubled_var.Ih = 2 * variance( Ih_vec );
+    doubled_var.P0 = 2 * variance( P0_vec );
+    doubled_var.h  = 2 * variance( h_vec );
+
+    return doubled_var;
 }
 
 
-// theta = each step in ABC produces one ParticleSet, which in turn is a vector of parameter combinations
-// omega = first index determines which ABC step, second index determines which particle the weight corresponds to
-// tau_sq = the variances of each of the parameters, for the last completed ABC set
-vector<double> predictive_prior_weights(ParticleSet predictive_prior, vector <double> old_weights, Tau_sq tau_sq) {
+vector<double> calculate_predictive_prior_weights(vector<Particle> predictive_prior, int set_num) {
 
-    double numerator = uniform_pdf(R0_min, R0_max)
-                       * uniform_pdf(Ih_min, Ih_max)
-                       * uniform_pdf(P0_min, P0_max)
-                       * uniform_pdf(h_min, h_max);
+    const int prior_size = predictive_prior.size();
+    vector<double> predictive_prior_weights( prior_size );
+    const double numerator = uniform_pdf(R0_min, R0_max)
+                           * uniform_pdf(Ih_min, Ih_max)
+                           * uniform_pdf(P0_min, P0_max)
+                           * uniform_pdf(h_min, h_max);
 
-    double denominator = 0.0;
-    //double normal_pdf(double x, double mu, double var) {
-    ParticleSet last_pset = theta[ theta.size() - 2 ];
-    Particle last_particle = theta.back().back();
+    vector <double>   old_predictive_prior_weights = read_weights_file( set_num - 2 ); 
+    vector<Particle>  old_predictive_prior         = read_predictive_prior_file( set_num - 2 );
+    const int old_prior_size                       = old_predictive_prior.size();
+    Doubled_variances old_par_2var                 = calculate_doubled_variances( old_predictive_prior );
 
-    cerr << "make sure that omega[0] is accessing the right weights list\n";
-    for (unsigned int j = 0; j < omega[ omega.size() - 2 ].size(); j++) {
-        denominator += omega[0][j] \
-                       * normal_pdf(last_particle.R0, last_pset[j].R0, tau_sq.R0)
-                       * normal_pdf(last_particle.Ih, last_pset[j].Ih, tau_sq.Ih)
-                       * normal_pdf(last_particle.P0, last_pset[j].P0, tau_sq.P0)
-                       * normal_pdf(last_particle.h,  last_pset[j].h, tau_sq.h);
+
+    for (int i = 0; i < prior_size; i++) {
+        double denominator = 0.0;
+        for (int j = 0; j < old_prior_size; j++) {
+            denominator += old_predictive_prior_weights[j]
+                           //double normal_pdf(double x, double mu, double var) {
+                           * normal_pdf(predictive_prior[i].R0, old_predictive_prior[j].R0, old_par_2var.R0)
+                           * normal_pdf(predictive_prior[i].Ih, old_predictive_prior[j].Ih, old_par_2var.Ih)
+                           * normal_pdf(predictive_prior[i].P0, old_predictive_prior[j].P0, old_par_2var.P0)
+                           * normal_pdf(predictive_prior[i].h,  old_predictive_prior[j].h,  old_par_2var.h);
+/*
+                 denominator = 1; // DEBUGGING, DELETE LATER
+                 cerr << predictive_prior[i].P0 
+                 << " " << old_predictive_prior[j].P0 
+                 << " " << old_par_2var.P0 
+                 << " " << normal_pdf(predictive_prior[i].P0, old_predictive_prior[j].P0, old_par_2var.P0)
+                 << endl;
+            cerr << old_predictive_prior_weights[j]
+                 << " " << normal_pdf(predictive_prior[i].R0, old_predictive_prior[j].R0, old_par_2var.R0)
+                 << " " << normal_pdf(predictive_prior[i].Ih, old_predictive_prior[j].Ih, old_par_2var.Ih)
+                 << " " << normal_pdf(predictive_prior[i].P0, old_predictive_prior[j].P0, old_par_2var.P0)
+                 << " " << normal_pdf(predictive_prior[i].h,  old_predictive_prior[j].h,  old_par_2var.h) << endl;
+                 */
+        }
+        predictive_prior_weights[i] = numerator / denominator;
     }
 
-    return numerator / denominator;
+    return normalize_dist( predictive_prior_weights );
 }
 
 
@@ -316,13 +433,17 @@ int main(int argc, char* argv[]) {
 
     // Determine what parameter combinations we will try
     static MTRand mtrand;
-    ParticleSet particles;
+    vector<Particle> particles;
     if (set_num == 0) {
         // sample naive priors
-        sample_prior(particles, par.sample_size, mtrand);
+        sample_prior(particles, par.sample_size, &mtrand);
     } else {
         // sample predictive priors based on last set
-        sample_predictive_prior(set_num, particles, par.sample_size, mtrand);
+        sample_predictive_prior(set_num, particles, par.sample_size, &mtrand);
+    }
+
+    for (unsigned int i = 0; i < particles.size(); i++) {
+        cerr << particles[i].R0 << " " << particles[i].Ih << " " << particles[i].h << " " << particles[i].P0 << endl;
     }
 
 exit(0);
@@ -331,7 +452,7 @@ exit(0);
     map<string, vector<float> > R0_vals;
     map<string, vector<float> > obs_data; //string = location, float = incidence on [0,1]
 
-    read_observed_data(par.observed_data_file, ',', obs_data);
+    load_observed_data(par.observed_data_file, ',', obs_data);
 
     Network* net = new Network("EpiNet", Network::Undirected);
     MultiSeason_Sim* sim = new MultiSeason_Sim(net, Ih);
@@ -387,7 +508,7 @@ void generate_network(Network* net, MultiSeason_Sim* sim) {
     sim->calc_naive_transmissibility(R_zero); //calculates correct T for this network topology
 }
 
-void read_observed_data(string filename, char sep, map<string, vector<float> > & obs_data) {
+void load_observed_data(string filename, char sep, map<string, vector<float> > & obs_data) {
     //cerr << "Reading observed data\n";
     ifstream myfile(filename.c_str());
 
